c1f3440d8914849f6dd1dc135749a2d9cd9ada9c
Konstantinos Kalogeropoulos <kkalo878@gmail.com>
2023-02-04 23:32:14 +0200

    Add Models

---
+++ TodoApiEFCore/TodoApiEFCore/TodoApi.Data/Models/TodosModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TodoApi.Data.Models;
+public class TodosModel
+{
+    [Key]
+    public int TodosId { get; set; }
+
+    [Required(ErrorMessage = "TodosName is required")]
+    [Column(TypeName = "nvarchar(100)")]
+    public string TodosName { get; set; }
+
+    [Required(ErrorMessage = "TodosDescription is required")]
+    [Column(TypeName = "nvarchar(100)")]
+    public string TodosDescription { get; set; }
+    
+    public List<TodoItemModel> Todoitems { get; set; }
+
+    [Required(ErrorMessage = "ItemStatus is required")]
+    [Column(TypeName = "bit")]
+    public bool TodosStatus { get; set; }
+}
